eaeb4997a8119de098c657603b4facdc3d5b28a5
Leonardo Oliveira <leonardo@ipeweb.com.br>
2023-04-08 23:01:22 -0300

    feat: links

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { Backend } from './components/Backend';
 import { Profile } from './components/Profile';
 import { Outros } from './components/Outros';
 
-
 function App() {
 	return (
 		<div className='m-auto max-w-[1520px] mb-9'>
